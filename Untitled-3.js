7cabcb61e4d8336d013134ba9f9b1070e8db83e3
YuJerolle <447408092@qq.com>
2018-04-25 15:34:41 +0800

    D

---
+++ Untitled-3.js
@@ -0,0 +1,39 @@
+
+	document.getElementById('b1').onclick = function(){
+		document.getElementById('d2').style.display = 'block';
+	}
+	document.getElementById('1').onclick = function(){
+		document.getElementById('d1').style.backgroundColor = 'red';
+	}
+	document.getElementById('2').onclick = function(){
+		document.getElementById('d1').style.backgroundColor = 'yellow';
+	}
+	document.getElementById('3').onclick = function(){
+		document.getElementById('d1').style.backgroundColor = 'blue';
+	}
+	document.getElementById('4').onclick = function(){
+		document.getElementById('d1').style.width = '200px';
+	}
+	document.getElementById('5').onclick = function(){
+		document.getElementById('d1').style.width = '300px';
+	}
+	document.getElementById('6').onclick = function(){
+		document.getElementById('d1').style.width = '400px';
+	}
+	document.getElementById('7').onclick = function(){
+		document.getElementById('d1').style.height = '200px';
+	}
+	document.getElementById('8').onclick = function(){
+		document.getElementById('d1').style.height = '300px';
+	}
+	document.getElementById('9').onclick = function(){
+		document.getElementById('d1').style.height = '400px';
+	}
+	document.getElementById('10').onclick = function(){
+		document.getElementById('d1').style.width = '100px';
+		document.getElementById('d1').style.height = '100px';
+		document.getElementById('d1').style.backgroundColor = 'white';
+	}
+	document.getElementById('11').onclick = function(){
+		document.getElementById('d2').style.display = 'none';
+	}
